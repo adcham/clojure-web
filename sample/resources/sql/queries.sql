1b1a21482538da48aa74091ddee7217d9ae4e6a9
Adam Chambers <b3017730@my.shu.ac.uk>
2022-01-18 12:55:30 +0000

    added sql queries and some data to the database

--- sample/resources/sql/queries.sql
+++ sample/resources/sql/queries.sql
@@ -1,21 +1,51 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+-- :name create-book! :! :n
+-- :doc creates a new book record
+INSERT INTO books
+(isbn, title, author)
+VALUES (:isbn, :title, :author)
 
--- :name update-user! :! :n
--- :doc updates an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
+-- :name update-book! :! :n
+-- :doc updates an existing book record
+UPDATE books
+SET isbn = :isbn, title = :title, author = :author
 WHERE id = :id
 
--- :name get-user :? :1
--- :doc retrieves a user record given the id
-SELECT * FROM users
+-- :name get-book :? :1
+-- :doc retrieves a book record given the id
+SELECT * FROM books
 WHERE id = :id
 
--- :name delete-user! :! :n
--- :doc deletes a user record given the id
-DELETE FROM users
+-- :name delete-book! :! :n
+-- :doc deletes a book record given the id
+DELETE FROM books
 WHERE id = :id
+
+-- :name get-books :? :*
+-- :doc retrieves all book records
+SELECT * FROM books
+
+-- :name create-review! :! :n
+-- :doc creates a new review record
+INSERT INTO reviews
+(book_id, reviewer, published, review)
+VALUES (:book_id, :reviewer, :published, :review)
+
+-- :name update-review! :! :n
+-- :doc updates an existing review record
+UPDATE reviews
+SET book_id = :book_id, reviewer = :reviewer, published = :published, review = :review
+WHERE id = :id
+
+-- :name get-review :? :1
+-- :doc retrieves a review record given the id
+SELECT * FROM reviews
+WHERE id = :id
+
+-- :name delete-review! :! :n
+-- :doc deletes a review record given the id
+DELETE FROM reviews
+WHERE id = :id
+
+-- :name get-reviews :? :*
+-- :doc retrieves all review records
+SELECT * FROM reviews
