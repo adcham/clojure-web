bedc672ed126449967094453d50e72c07cdb93c5
Adam Chambers <b3017730@my.shu.ac.uk>
2022-01-21 12:21:03 +0000

    validated input and reviewed styling for reviews page

--- sample/src/clj/sample/routes/home.clj
+++ sample/src/clj/sample/routes/home.clj
@@ -5,13 +5,39 @@
    [clojure.java.io :as io]
    [sample.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :as response]
+    [struct.core :as stc]))
 
 (defn home-page [request]
   (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
 
 (defn about-page [request]
   (layout/render request "about.html"))
+
+(def review-schema
+    {:book_id  [stc/required stc/number-str]
+     :reviewer [stc/required stc/string]
+     :review   [stc/required stc/string]})
+
+(defn validate-review
+    [params]
+    (first (stc/validate params review-schema)))
+
+(defn save-review [{:keys [params]}]
+    (if-let [errors (validate-review params)]
+        (-> (response/found "/reviews")
+            (assoc :flash (assoc params :errors errors)))
+        (do
+            (db/create-review! params)
+            (response/found "/reviews"))))
+
+(defn reviews-page [{:keys [flash] :as request}]
+  (layout/render
+      request
+      "reviews.html"
+      (merge
+          {:items (db/get-reviews)}
+          (select-keys flash [:book_id :review :reviewer :errors]))))
 
 (defn home-routes []
   [ "" 
@@ -29,3 +55,4 @@
 
 (defn reviews-page [request]
   (layout/render request "reviews.html" {:items (db/get-reviews)}))
+
